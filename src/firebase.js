4b3ffa3273911b986ff103ce1af57e5532867fe0
the-PrafulDesai <the.prafuldesai@gmail.com>
2024-04-16 15:48:50 +0530

    1st commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZfBhUCYA-gTu8vTzwvnrX4fbYzE77OUc",
+  authDomain: "grofersecommerce.firebaseapp.com",
+  projectId: "grofersecommerce",
+  storageBucket: "grofersecommerce.appspot.com",
+  messagingSenderId: "1096954243515",
+  appId: "1:1096954243515:web:666ee09972cb2187a67c9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
